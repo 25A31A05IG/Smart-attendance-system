2443d4b24061a28e1b06359412ac1528802b432c
Netheti Ramesh <rameshnetheti2008@gmail.com>
2026-07-15 19:36:51 +0530

    axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,23 +1,17 @@
 import axios from "axios";
 
 const API = axios.create({
-
-    baseURL: import.meta.env.VITE_API_URL
-
+  baseURL: "https://smart-attendance-system-ydti.onrender.com/api",
 });
 
 API.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
 
-    const token = localStorage.getItem("token");
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
 
-    if (token) {
-
-        config.headers.Authorization = `Bearer ${token}`;
-
-    }
-
-    return config;
-
+  return config;
 });
 
 export default API;
