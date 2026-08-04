5de79884c57cf55f0a8d470bfae976a1ecf5b0b6
Netheti Ramesh <rameshnetheti2008@gmail.com>
2026-08-04 15:20:02 +0530

    dashboardRoutes.js

--- server/routes/dashboardRoutes.js
+++ server/routes/dashboardRoutes.js
@@ -1,16 +1,13 @@
 const express = require("express");
-
 const router = express.Router();
 
+const authMiddleware = require("../middleware/authMiddleware");
 
 const {
     getDashboardData
 } = require("../controllers/dashboardController");
 
-
-
-router.get("/", getDashboardData);
-
-
+// Protect dashboard route
+router.get("/", authMiddleware, getDashboardData);
 
 module.exports = router;
