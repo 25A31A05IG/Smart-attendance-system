10de3155eea3ce6f3e0a78c5b5ead3890b2747c5
Netheti Ramesh <rameshnetheti2008@gmail.com>
2026-08-20 19:27:49 +0530

    studentRoutes.js

--- server/routes/studentRoutes.js
+++ server/routes/studentRoutes.js
@@ -2,9 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
-const upload = require("../config/upload");
+const upload =
+  require("../config/upload");
 
-const authMiddleware = require("../middleware/authMiddleware");
+const authMiddleware =
+  require("../middleware/authMiddleware");
 
 const {
   createStudent,
@@ -13,47 +15,35 @@ const {
   uploadFaceImage,
 } = require("../controllers/studentController");
 
-// ==========================================
-// Protect all student routes
-// ==========================================
 
-router.use(authMiddleware);
+router.use(
+  authMiddleware
+);
 
-// ==========================================
-// Create Student
-// ==========================================
 
 router.post(
   "/",
   createStudent
 );
 
-// ==========================================
-// Get Logged-in User Students
-// ==========================================
 
 router.get(
   "/",
   getAllStudents
 );
 
-// ==========================================
-// Delete Student
-// ==========================================
 
 router.delete(
   "/:id",
   deleteStudent
 );
 
-// ==========================================
-// Register / Update Face
-// ==========================================
 
 router.post(
   "/upload/:id",
   upload.single("faceImage"),
   uploadFaceImage
 );
+
 
 module.exports = router;
